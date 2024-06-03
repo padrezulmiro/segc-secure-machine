d1011a8bf51606691b8d182650b136bbbd076d56
aZul <fmdcosta.piano@gmail.com>
2024-06-03 04:22:44 +0100

    feat: Implement R5 and R7

--- iptables-setup.sh
+++ iptables-setup.sh
@@ -3,6 +3,7 @@
 iotserver_port="12345"
 gcc_ip="10.101.151.5"
 dcs_subnet="10.101.52.0/27"
+internal_subnet="10.101.85.0/24"
 
 # Reset INPUT, OUTPUT and FORWARD to default, i.e. accept all
 iptables -P INPUT ACCEPT
@@ -17,12 +18,12 @@ iptables --append INPUT --protocol tcp --dport $iotserver_port --match state \
 iptables --append OUTPUT --protocol tcp --sport $iotserver_port --match state \
     --state ESTABLISHED --jump ACCEPT
 
-# R2 R8 - Accept SSH but only from GCC
+# R2 R5 R8 - Accept SSH but only from GCC, and can initiate it to SSH too
 # SSH runs on top of TCP/IP stack, 22 is the SSH reserved port
 iptables --append INPUT --protocol tcp --source $gcc_ip --dport 22 \
     --match state --state NEW,ESTABLISHED --jump ACCEPT
 iptables --append OUTPUT --protocol tcp --destination $gcc_ip --sport 22 \
-    --match state --state ESTABLISHED --jump ACCEPT
+    --match state --state NEW,ESTABLISHED --jump ACCEPT
 
 # R8 - Accept SSH from subnet that includes DC1, 2 and 3
 # 10.101.52.0/27 implies a 255.255.255.224 subnet mask
@@ -31,9 +32,11 @@ iptables --append INPUT --protocol tcp --source $dcs_subnet --dport 22 \
 iptables --append OUTPUT --protocol tcp --destination $dcs_subnet --sport 22 \
     --match state --state ESTABLISHED --jump ACCEPT
 
-# R7
-
-# R8
+# R7 - Only receive and respond to ping requests from GCC and the internal subnet
+iptables --append INPUT --protocol icmp --icmp-type echo-request \
+    --source $gcc_ip,$internal_subnet --jump ACCEPT
+iptables --append OUTPUT --protocol icmp --icmp-type echo-reply \
+    --destination $gcc_ip,$internal_subnet --jump ACCEPT
 
 # R9
 
